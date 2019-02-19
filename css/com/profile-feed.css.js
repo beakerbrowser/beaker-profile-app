1428ec6cdd8e6c36c4d4cd891a881848cd02d2bc
Paul Frazee <pfrazee@gmail.com>
2019-02-19 08:11:20 -0600

    Factor out feed-post component

--- css/com/profile-feed.css.js
+++ css/com/profile-feed.css.js
@@ -1,66 +1,17 @@
 import {css} from '/vendor/beaker-app-stdlib/vendor/lit-element/lit-element.js'
-import buttonsCSS from '/vendor/beaker-app-stdlib/css/buttons.css.js'
 
 const cssStr = css`
-${buttonsCSS}
-
-a:hover {
-  text-decoration: underline;
-}
-
 :host {
   display: block;
   border: 1px solid #ddd;
 }
 
-profile-feed-post {
-  display: flex;
-  width: 100%;
-  background: #fff;
-  padding: 14px;
+feed-post {
   border-bottom: 1px solid #ddd;
 }
 
-profile-feed-post:last-of-type {
+feed-post:last-of-type {
   border-bottom: 0;
-}
-
-.avatar-column {
-  width: 64px;
-}
-
-.content-column {
-  flex: 1;
-}
-
-.avatar {
-  display: block;
-  width: 50px;
-  height: 50px;
-  border-radius: 50%;
-}
-
-.header {
-  margin-bottom: 8px;
-  font-weight: 500;
-  font-size: 14px;
-  color: var(--color-text--muted);
-}
-
-.title {
-  font-weight: bold;
-  color: var(--color-text);
-}
-
-.domain,
-.permalink {
-  color: inherit;
-}
-
-.body {
-  white-space: pre;
-  font-size: 14px;
-  color: var(--color-text--dark);
 }
 `
 export default cssStr
--- js/com/profile-feed.js
+++ js/com/profile-feed.js
@@ -1,47 +1,20 @@
 import {LitElement, html} from '/vendor/beaker-app-stdlib/vendor/lit-element/lit-element.js'
-import profileFeedNav from '../../css/com/profile-feed.css.js'
+import {FeedPost} from '/vendor/beaker-app-stdlib/js/com/feed/post.js'
+import profileFeedCSS from '../../css/com/profile-feed.css.js'
 
 class ProfileFeed extends LitElement {
   render () {
     return html`
-      <profile-feed-post></profile-feed-post>
-      <profile-feed-post></profile-feed-post>
-      <profile-feed-post></profile-feed-post>
-      <profile-feed-post></profile-feed-post>
-      <profile-feed-post></profile-feed-post>
-      <profile-feed-post></profile-feed-post>
-      <profile-feed-post></profile-feed-post>
+      <feed-post></feed-post>
+      <feed-post></feed-post>
+      <feed-post></feed-post>
+      <feed-post></feed-post>
+      <feed-post></feed-post>
+      <feed-post></feed-post>
+      <feed-post></feed-post>
     `
   }
 }
-ProfileFeed.styles = profileFeedNav
+ProfileFeed.styles = profileFeedCSS
+FeedPost.register()
 customElements.define('profile-feed', ProfileFeed)
-
-class ProfileFeedPost extends LitElement {
-  createRenderRoot() {
-    return this // dont use the shadow dom, let ProfileFeed style us
-  }
-
-  render () {
-    return html`
-      <div class="avatar-column">
-        <img class="avatar" src="/img/tmp-profile.png">
-      </div>
-      <div class="content-column">
-        <div class="header">
-          <a class="title" href="#">Paul Frazee</a>
-          <a class="domain" href="#">pfrazee.com</a>
-          &middot;
-          <a class="permalink" href="#">1h</a>
-        </div>
-        <div class="body">Was I out of my head
-Was I out of my mind?
-How could I have forgotten to .bind
-I ‘await’ed for an invocation
-“this.load is undefined”</div>
-      </div>
-    `
-  }
-}
-ProfileFeedPost.styles = profileFeedNav
-customElements.define('profile-feed-post', ProfileFeedPost)
